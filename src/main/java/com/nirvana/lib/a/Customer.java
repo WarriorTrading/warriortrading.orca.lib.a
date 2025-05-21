b29b31aed6a611b55b9a43004eb0f3e8d158ca64
yosaku04 <rick.wan@nirvana-info.com>
2025-05-21 17:39:26 +0800

    Add email property

--- src/main/java/com/nirvana/lib/a/Customer.java
+++ src/main/java/com/nirvana/lib/a/Customer.java
@@ -3,6 +3,7 @@ package com.nirvana.lib.a;
 public class Customer {
     private Long id;
     private String name;
+    private String email;
 
     public Customer() {}
 
@@ -31,7 +32,15 @@ public class Customer {
     public String toString() {
         return "Customer{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", name='" + name + '\'' + ", email='" + email + '\'' +
                 '}';
     }
-} 
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
